73dcf1ee14eedefb0e8c0f5acf79584761196119
Deeksha Sharma <deeksharma14042001@gmail.com>
2023-12-06 00:51:14 +0800

    full solution for codeforces round 886 div 3

---
+++ codeforces/cf-4-886/g.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define ss second
+#define ff first
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define mii map<int,int>
+#define si set <int>
+#define vi vector <int>
+#define vpi vector <pii>
+#define vpp vector <pair<int, pii>>
+#define mpi map <pii, int>
+#define spi set <pii>
+#define double long double
+#define que_max priority_queue <int>
+#define que_min priority_queue <int, vi, greater<int>>
+
+#define loop(i, l, r) for(int i = l;i < r ; i++)
+#define rev(i, n, x) for(int i = n; i >= x; i--)
+
+#define mod 1000000007
+#define pi 3.14159265359
+
+#define sz size()
+#define all(v) v.begin(),v.end()
+#define endl "\n"
+
+#define io ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+// printing values
+#define print(a) for(auto x : a) cout << x << " "; cout << endl
+#define printpair(a) for(auto x : a) cout << x.ff << " " << x.ss << endl
+#define printloop(a,x,y) for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+
+// debugging
+#define cloop(i,x,y) for(int i=(x);i<(y);++i)
+#define varbug(a) cout << #a << " = " << a << endl;
+#define loopbug(a, x, y) { cout << #a << ": "; cloop(i, x, y) cout << a[i] << ' '; cout << endl; }
+
+const int N = 200005;
+
+int dir4[4][2] = {{1, 0}, { -1, 0}, {0, -1}, {0, 1}};
+int dir8[8][2] = {{1, 0}, { -1, 0}, {0, -1}, {0, 1}, {1, 1}, { -1, 1}, {1, -1}, { -1, -1}};
+
+int power(int a, int b) {
+	if (b == 0) return 1;
+	if (b % 2) return (a * (power((a * a) % mod, b / 2) % mod)) % mod;
+	else return power((a * a) % mod, b / 2) % mod;
+}
+
+void solve() {
+	int n;
+	cin>>n;
+	map<int,int> xx, yy, x_plus_y, x_minus_y;
+	vector<pii > vp;
+	for(int i=0;i<n;i++){
+		int x, y;
+		cin>>x>>y;
+		vp.pb(mp(x, y));
+		xx[x]++;
+		yy[y]++;
+		x_plus_y[x+y]++;
+		x_minus_y[x-y]++;
+	}
+
+	int ans = 0;
+	for(int i=0;i<n;i++){
+		int x = vp[i].ff;
+		int y = vp[i].ss;
+		ans += xx[x] - 1 + yy[y] -1 + x_plus_y[x+y] -1 + x_minus_y[x-y] -1;
+	}
+	cout<<ans<<endl;
+}
+
+int32_t main() {
+	io;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+
+	int t = 1;
+	cin >> t;
+	double time1 = (double)clock() / CLOCKS_PER_SEC;
+	while (t--) solve();
+
+
+	double time2 = (double)clock() / CLOCKS_PER_SEC;
+	cerr << "time taken : " << time2 - time1 << " secs" << endl;
+}
---
+++ codeforces/cf-4-886/h.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define ss second
+#define ff first
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define mii map<int,int>
+#define si set <int>
+#define vi vector <int>
+#define vpi vector <pii>
+#define vpp vector <pair<int, pii>>
+#define mpi map <pii, int>
+#define spi set <pii>
+#define double long double
+#define que_max priority_queue <int>
+#define que_min priority_queue <int, vi, greater<int>>
+
+#define loop(i, l, r) for(int i = l;i < r ; i++)
+#define rev(i, n, x) for(int i = n; i >= x; i--)
+
+#define mod 1000000007
+#define pi 3.14159265359
+
+#define sz size()
+#define all(v) v.begin(),v.end()
+#define endl "\n"
+
+#define io ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+// printing values
+#define print(a) for(auto x : a) cout << x << " "; cout << endl
+#define printpair(a) for(auto x : a) cout << x.ff << " " << x.ss << endl
+#define printloop(a,x,y) for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+
+// debugging
+#define cloop(i,x,y) for(int i=(x);i<(y);++i)
+#define varbug(a) cout << #a << " = " << a << endl;
+#define loopbug(a, x, y) { cout << #a << ": "; cloop(i, x, y) cout << a[i] << ' '; cout << endl; }
+
+const int N = 200005;
+
+int dir4[4][2] = {{1, 0}, { -1, 0}, {0, -1}, {0, 1}};
+int dir8[8][2] = {{1, 0}, { -1, 0}, {0, -1}, {0, 1}, {1, 1}, { -1, 1}, {1, -1}, { -1, -1}};
+
+int power(int a, int b) {
+	if (b == 0) return 1;
+	if (b % 2) return (a * (power((a * a) % mod, b / 2) % mod)) % mod;
+	else return power((a * a) % mod, b / 2) % mod;
+}
+
+void solve() {
+	int n, m;
+	cin>>n>>m;
+
+	vector<vector<pair<int, int> > > g(n+1);
+
+	for(int i=0;i<m;i++){
+		int a, b, d;
+		cin>>a>>b>>d;
+
+		g[b].pb(mp(a, -d));
+		g[a].pb(mp(b, d));
+	}
+
+	vector<int> vis(n+1, false);
+	vector<int> val(n+1, 0);
+	for(int i=1;i<=n;i++){
+		if(!vis[i]){
+			queue<int> q;
+			q.push(i);
+			vis[i] = true;
+			while(!q.empty()){
+				int x = q.front();
+				q.pop();
+				for(auto child: g[x]){
+					if(vis[child.ff]){
+						if(val[child.ff]!=val[x]+child.ss){
+							cout<<"NO"<<endl;
+							return;
+						}
+					}else{
+						q.push(child.ff);
+						vis[child.ff] = true;
+						val[child.ff] = val[x]+child.ss;
+					}
+				}
+			}
+		}
+	}
+	cout<<"YES"<<endl;
+}
+
+int32_t main() {
+	io;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+
+
+	int t = 1;
+	cin >> t;
+	double time1 = (double)clock() / CLOCKS_PER_SEC;
+	while (t--) solve();
+
+
+	double time2 = (double)clock() / CLOCKS_PER_SEC;
+	cerr << "time taken : " << time2 - time1 << " secs" << endl;
+}
